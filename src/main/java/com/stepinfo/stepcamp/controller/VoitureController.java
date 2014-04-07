34b42681405f4d77e770d1a8c59824d6f1215b91
Francois <francois.nollen@stepinfo.com>
2014-04-07 22:50:56 +0200

    Securisation du garage

--- src/main/java/com/stepinfo/stepcamp/controller/VoitureController.java
+++ src/main/java/com/stepinfo/stepcamp/controller/VoitureController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -40,6 +41,7 @@ public class VoitureController {
 
     //Préparation de l'ajout d'une voiture
     @RequestMapping(value = "/prepareAjout", method = RequestMethod.GET)
+    @Secured("ROLE_ADMIN")
 	public String prepareAjoutVoiture(Voiture voiture, Model model) {
         model.addAttribute("voiture", voitureService.getNouvelleVoiture());
         idVoitureMaj = 0;
--- src/main/webapp/WEB-INF/jsp/include/footer.jsp
+++ src/main/webapp/WEB-INF/jsp/include/footer.jsp
@@ -1,3 +1,4 @@
 <footer>
     &middot; &copy; 2014 e-Paye tous droits r&eacute;serv&eacute;s &middot;
+    <a id="logout" href="/garage/j_spring_security_logout">Deconnexion</a>
 </footer>
